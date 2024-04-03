e61dde7f6dce891a4e452d23088d9c69f109310d
Daniel <daniel.passage606@passmail.com>
2024-04-03 14:02:31 -0400

    added borrowingFactor

--- test/unit/GMXLensV2.t.sol
+++ test/unit/GMXLensV2.t.sol
@@ -26,12 +26,17 @@ interface IMarketStoreUtils {
     function SHORT_TOKEN() external view returns (bytes32);
 }
 
+interface IPositionStoreUtils {
+    function BORROWING_FACTOR() external view returns (bytes32);
+}
+
 contract GMXLensV2Test is Test {
     GMXLensV2 public lens;
     IReader public reader;
     IOrderStoreUtils public orderStoreUtils;
     IDepositStoreUtils public depositStoreUtils;
     IMarketStoreUtils public marketStoreUtils;
+    IPositionStoreUtils public positionStoreUtils;
 
     struct MarketDataState {
         address marketToken;
@@ -45,11 +50,11 @@ contract GMXLensV2Test is Test {
         orderStoreUtils = IOrderStoreUtils(0x97BeB5A20FBd4596c8B19a89Ec399a100e57d14d);
         depositStoreUtils = IDepositStoreUtils(0x98e86155abf8bCbA566b4a909be8cF4e3F227FAf);
         marketStoreUtils = IMarketStoreUtils(0x5a1344252f0CdfDB765DD5ab97C98734f1D7ED6d);
+        positionStoreUtils = IPositionStoreUtils(0x4a57C9b3d6c96954e397Cc186F98fCD2816A95C7);
         // lens.initialize();
     }
 
     function testGetMarketData() public {
-        uint256 poolValueExpected;
 
         uint256 poolValue = reader.poolAmounts(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
         emit log("poolValue:");
@@ -81,6 +86,11 @@ contract GMXLensV2Test is Test {
         address shortTokennAddress = address(uint160(uint256(shortToken)));
         emit log("shortTokennAddress:");
         emit log_address(shortTokennAddress);
+        
+        bytes32 borrowingFactor = positionStoreUtils.BORROWING_FACTOR();
+        address borrowingFactorAddress = address(uint160(uint256(borrowingFactor)));
+        emit log("borrowingFactorAddress:");
+        emit log_address(borrowingFactorAddress);
     }
 }
 
