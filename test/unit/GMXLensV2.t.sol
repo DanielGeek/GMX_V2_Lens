1034937667df699d1e30f1b6d848db54cb66a28e
Daniel <daniel.passage606@passmail.com>
2024-04-03 12:49:06 -0400

    Add more info

--- test/unit/GMXLensV2.t.sol
+++ test/unit/GMXLensV2.t.sol
@@ -20,11 +20,18 @@ interface IDepositStoreUtils {
     function INITIAL_SHORT_TOKEN_AMOUNT() external view returns (bytes32);
 }
 
+interface IMarketStoreUtils {
+    function INDEX_TOKEN() external view returns (bytes32);
+    function LONG_TOKEN() external view returns (bytes32);
+    function SHORT_TOKEN() external view returns (bytes32);
+}
+
 contract GMXLensV2Test is Test {
     GMXLensV2 public lens;
     IReader public reader;
     IOrderStoreUtils public orderStoreUtils;
     IDepositStoreUtils public depositStoreUtils;
+    IMarketStoreUtils public marketStoreUtils;
 
     struct MarketDataState {
         address marketToken;
@@ -37,6 +44,7 @@ contract GMXLensV2Test is Test {
         reader = IReader(0x489ee077994B6658eAfA855C308275EAd8097C4A);
         orderStoreUtils = IOrderStoreUtils(0x97BeB5A20FBd4596c8B19a89Ec399a100e57d14d);
         depositStoreUtils = IDepositStoreUtils(0x98e86155abf8bCbA566b4a909be8cF4e3F227FAf);
+        marketStoreUtils = IMarketStoreUtils(0x5a1344252f0CdfDB765DD5ab97C98734f1D7ED6d);
         // lens.initialize();
     }
 
@@ -53,10 +61,26 @@ contract GMXLensV2Test is Test {
         emit log("longToken:");
         emit log_address(isLongAddress);
 
-        bytes32 shortToken = depositStoreUtils.INITIAL_SHORT_TOKEN();
-        address shortTokenAddress = address(uint160(uint256(shortToken)));
-        emit log("shortTokenAddress:");
-        emit log_address(shortTokenAddress);
+        bytes32 initialShortToken = depositStoreUtils.INITIAL_SHORT_TOKEN();
+        address initialShortTokenAddress = address(uint160(uint256(initialShortToken)));
+        emit log("initialShortTokenAddress:");
+        emit log_address(initialShortTokenAddress);
+
+
+        bytes32 indexToken = marketStoreUtils.INDEX_TOKEN();
+        address indexTokenAddress = address(uint160(uint256(indexToken)));
+        emit log("indexTokenAddress:");
+        emit log_address(indexTokenAddress);
+
+        bytes32 longToken = marketStoreUtils.LONG_TOKEN();
+        address longTokenAddress = address(uint160(uint256(longToken)));
+        emit log("longTokenAddress:");
+        emit log_address(longTokenAddress);
+        
+        bytes32 shortToken = marketStoreUtils.SHORT_TOKEN();
+        address shortTokennAddress = address(uint160(uint256(shortToken)));
+        emit log("shortTokennAddress:");
+        emit log_address(shortTokennAddress);
     }
 }
 
